f013270805fde29f91bf2738d8db75db67f9620e
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-11-11 16:01:25 +0600

    fix: spacing

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -70,18 +70,18 @@ export default function Page() {
                 <div className="h-0.5 bg-neutral-600 my-12"></div>
                 <div>
                   <div className='md:flex'>
-                    <div className='w-1/2 space-y-6'>
+                    <div className='md:w-1/2 space-y-6'>
                       <h1>Name:<span className='text-neutral-400 pl-4'>Sabbir Ahmed</span></h1>
                       <h1>Email:<span className='text-[#DCC5B2] pl-4'>sabbir89654@gmail.com</span></h1>
                     </div>
-                    <div className='w-1/2 space-y-6 mt-4 md:mt-0 '>
+                    <div className='md:w-1/2 space-y-6 mt-4 md:mt-0 '>
                       <h1>Age:<span className='text-neutral-400 pl-4'>N/A</span></h1>
                       <h1>From:<span className='text-neutral-400 pl-4'>Kushtia, Bangladesh</span></h1>
                     </div>
                   </div>
                   <div className='mt-10 md:flex space-y-6 md:space-y-0 justify-between items-center w-fit'>
                     <Link href="/" className='px-8 py-2 rounded-3xl bg-linear-to-l from-[#DCC5B2] to-transparent'>Download CV</Link>
-                    <div className='border-t-2 border-neutral-600 w-20 mx-10'></div>
+                    <div className='border-t-2 border-neutral-600 my-10 md:my-0 w-20 mx-10'></div>
                     <div className="flex gap-4 text-neutral-400">
                       <a href="https://twitter.com" target="_blank" className="hover:text-blue-400 transition">
                         <Twitter size={24} />
