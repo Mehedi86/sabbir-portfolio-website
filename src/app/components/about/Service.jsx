010faccecf6019cea104cc80bbe02b9b4d0c92f5
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-11-12 11:27:05 +0600

    fix: responsiveness

--- src/app/components/about/Service.jsx
+++ src/app/components/about/Service.jsx
@@ -9,7 +9,7 @@ export default function Service() {
                 title="My Services"
                 subtitle="Offer to my clients"
             />
-            <div className="grid md:grid-cols-3 gap-6 mt-12">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 {services.map((service) => (
                     <div key={service.id} className="bg-neutral-800 h-[212px] p-8 shadow-lg hover:border-b-2 border-[#DCC5B2]">
                         <div className="mb-4">{service.icon}</div>
